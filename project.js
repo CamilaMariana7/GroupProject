7fa15629fc43b296680768db231667e427290303
CamilaMariana7 <122727974+CamilaMariana7@users.noreply.github.com>
2023-02-02 20:42:58 +0000

    Add files via upload

---
+++ project.js
@@ -0,0 +1,70 @@
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+
+var wm = document.getElementById('w_minutes');
+var ws = document.getElementById('w_seconds');
+
+var bm = document.getElementById('b_minutes');
+var bs = document.getElementById('b_seconds');
+
+
+var startTimer;
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Don't rush! The tool is working!"); //We changed the alert
+    }
+})
+
+reset.addEventListener('click', function(){
+    wm.innerText = 20;
+    ws.innerText = "00";
+// We changed the time because it was initially 25 minutes and 5 minutes break
+    bm.innerText = 3;
+    bs.innerText = "00";
+//nothing changed from onwards
+    document.getElementById('counter').innerText = 0;
+    stopInterval()
+    startTimer = undefined;
+})
+
+stop.addEventListener('click', function(){
+    stopInterval()
+    startTimer = undefined;
+})
+
+
+function timer(){
+
+    if(ws.innerText != 0){
+        ws.innerText--;
+    } else if(wm.innerText != 0 && ws.innerText == 0){
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    if(wm.innerText == 0 && ws.innerText == 0){
+        if(bs.innerText != 0){
+            bs.innerText--;
+        } else if(bm.innerText != 0 && bs.innerText == 0){
+            bs.innerText = 59;
+            bm.innerText--;
+        }
+    }
+    if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
+        wm.innerText = 20;
+        ws.innerText = "00";
+
+        bm.innerText = 3;
+        bs.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+    }
+}
+
+function stopInterval(){
+    clearInterval(startTimer);
+}
